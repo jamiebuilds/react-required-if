c1066098d12c3792298ccc8fe1eb125097a33bd0
Edward Bramanti <edward@bramanti.org>
2017-07-31 23:03:13 -0700

    Enforce valid type/condition This provides better messaging when an invalid type or condition is provided.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 module.exports = function requiredIf(type, condition) {
-  return function(props) {
+  return function(props, propName, componentName) {
+    if (typeof type !== "function") {
+      return new Error(
+        `Invalid react-required-if prop type supplied to ${componentName}. Validation failed.`
+      );
+    }
+
+    if (typeof condition !== "function") {
+      return new Error(
+        `Invalid react-required-if condition supplied to ${componentName}. Validation failed.`
+      );
+    }
+
     var test = condition(props) ? type.isRequired : type;
     return test.apply(this, arguments);
   };
