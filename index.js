17d2827dba5d077d10f5a9c0ec852490ef008742
Dmitry Olkhovoi <auuine@gmail.com>
2017-10-27 17:44:25 +0300

    Update index.js
    To support ES5 env, because you don't build your code before publish to npm, pls merge it

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ module.exports = function requiredIf(type, condition) {
   return function(props, propName, componentName) {
     if (typeof type !== "function") {
       return new Error(
-        `Invalid react-required-if prop type supplied to ${componentName}. Validation failed.`
+        'Invalid react-required-if prop type supplied to ' + componentName + '. Validation failed.'
       );
     }
 
     if (typeof condition !== "function") {
       return new Error(
-        `Invalid react-required-if condition supplied to ${componentName}. Validation failed.`
+        'Invalid react-required-if condition supplied to ' + componentName + '. Validation failed.'
       );
     }
 
